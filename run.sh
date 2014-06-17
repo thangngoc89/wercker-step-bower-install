c65af5971a07df8212223f68085df533e1780433
leipert <git@valedictorum.de>
2014-06-17 10:44:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 export BOWER_STORAGE__CACHE="${WERCKER_CACHE_DIR}/wercker/bower"
 mkdir -p $BOWER_STORAGE__CACHE
-bower install
+mkdir -p $BOWER_STORAGE__CACHE/.bower-cache
+mkdir -p $BOWER_STORAGE__CACHE/.bower-registry
+mkdir -p $BOWER_STORAGE__CACHE/.bower-tmp
+bower install --config.storage.packages=$BOWER_STORAGE__CACHE/.bower-cache --config.storage.registry=$BOWER_STORAGE__CACHE/.bower-registry --config.tmp=$BOWER_STORAGE__CACHE/.bower-tmp 
