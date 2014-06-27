5fd6711669735c5284fa5ff1aae8ca6566870ce5
Alexander Plesovskikh <me@hgen.ru>
2014-06-27 14:20:33 +0600

    fix interactive question

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p $BOWER_STORAGE__CACHE
 mkdir -p $BOWER_STORAGE__CACHE/.bower-cache
 mkdir -p $BOWER_STORAGE__CACHE/.bower-registry
 mkdir -p $BOWER_STORAGE__CACHE/.bower-tmp
-bower install --config.storage.packages=$BOWER_STORAGE__CACHE/.bower-cache --config.storage.registry=$BOWER_STORAGE__CACHE/.bower-registry --config.tmp=$BOWER_STORAGE__CACHE/.bower-tmp 
+bower install --config.interactive=false --config.storage.packages=$BOWER_STORAGE__CACHE/.bower-cache --config.storage.registry=$BOWER_STORAGE__CACHE/.bower-registry --config.tmp=$BOWER_STORAGE__CACHE/.bower-tmp 
